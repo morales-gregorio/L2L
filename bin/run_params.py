a897ce41be803364f7b81138a94225b83918aba3
Paulina Dabrowska <p.dabrowska@fz-juelich.de>
2020-11-03 17:12:41 +0100

    added the file with extracted parameters

---
+++ bin/run_params.py
@@ -0,0 +1,20 @@
+# parameters for the l2l-comet-[..]-ga.py script
+
+# Optimizee params
+optimizee_params = {
+    'seed': 123,
+    'keys_to_evolve': ['P_EE', 'P_EI', 'P_IE', 'P_II']
+}
+
+# Outer-loop optimizer initialization
+optimizer_params = {
+    'seed': 123,
+    'popsize': 24,
+    'CXPB': 0.8,
+    'MUTPB': 0.002,
+    'NGEN': 50,
+    'indpb': 0.2,
+    'tournsize': 3,
+    'matepar': 0.5,
+    'mutpar': 0.05
+}
