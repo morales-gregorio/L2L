d89158dad3b470d66456278ec1a1116ea8ec2762
Aitor <a.morales-gregorio@fz-juelich.de>
2020-11-05 12:11:57 +0100

    include boilerplate cleaning changes by alper

--- bin/l2l-comet-ga.py
+++ bin/l2l-comet-ga.py
@@ -1,116 +1,25 @@
-import logging.config
-import os
-
 from datetime import datetime
 from getpass import getuser
 
-from l2l.utils.environment import Environment
-
-from l2l.logging_tools import create_shared_logger_data, configure_loggers
 from l2l.optimizees.comet.optimizee import CometOptimizee, \
     CometOptimizeeParameters
 from l2l.optimizers.evolution import GeneticAlgorithmOptimizer,\
     GeneticAlgorithmParameters
-from l2l.paths import Paths
-import l2l.utils.JUBE_runner as jube
+from l2l.utils.experiment import Experiment
 
 from comet.evaluation.joint_test import joint_test
 
 import argparse
 
-logger = logging.getLogger('bin.l2l-comet')
-
 
 def run_experiment(args):
+    experiment = Experiment(root_dir_path='../results')
     name = 'L2L-COMET-{}-{}'.format(getuser(),
                                     datetime.now().strftime("%Y-%m-%d-%H_%M"))
-    try:
-        with open('path.conf') as f:
-            root_dir_path = f.read().strip()
-    except FileNotFoundError:
-        raise FileNotFoundError(
-            "You have not set the root path to store your results."
-            " Write the path to a path.conf text file in the bin directory"
-            " before running the simulation")
-
     trajectory_name = 'comet'
-
-    paths = Paths(name, dict(run_num='test'), root_dir_path=root_dir_path,
-                  suffix="-" + trajectory_name)
-
-    print("All output logs can be found in directory ", paths.logs_path)
-
-    # Create an environment that handles running our simulation
-    # This initializes an environment
-    env = Environment(
-        trajectory=trajectory_name,
-        filename=paths.output_dir_path,
-        file_title='{} data'.format(name),
-        comment='{} data'.format(name),
-        add_time=True,
-        automatic_storing=True,
-        log_stdout=False,  # Sends stdout to logs
-    )
-
-    create_shared_logger_data(
-        logger_names=['bin', 'optimizers'],
-        log_levels=['INFO', 'INFO'],
-        log_to_consoles=[True, True],
-        sim_name=name,
-        log_directory=paths.logs_path)
-    configure_loggers()
-
-    # Get the trajectory from the environment
-    traj = env.trajectory
-
-    # Set JUBE params
-    traj.f_add_parameter_group("JUBE_params", "Contains JUBE parameters")
-
-    # Scheduler parameters
-    # Name of the scheduler
-    # traj.f_add_parameter_to_group("JUBE_params", "scheduler", "Slurm")
-    # Command to submit jobs to the schedulers
-    traj.f_add_parameter_to_group("JUBE_params", "submit_cmd", "sbatch")
-    # Template file for the particular scheduler
-    traj.f_add_parameter_to_group("JUBE_params", "job_file", "job.run")
-    # Number of nodes to request for each run
-    traj.f_add_parameter_to_group("JUBE_params", "nodes", "1")
-    # Requested time for the compute resources
-    traj.f_add_parameter_to_group("JUBE_params", "walltime", "01:00:00")
-    # MPI Processes per node
-    traj.f_add_parameter_to_group("JUBE_params", "ppn", "1")
-    # CPU cores per MPI process
-    traj.f_add_parameter_to_group("JUBE_params", "cpu_pp", "1")
-    # Threads per process
-    traj.f_add_parameter_to_group("JUBE_params", "threads_pp", "4")
-    # Type of emails to be sent from the scheduler
-    traj.f_add_parameter_to_group("JUBE_params", "mail_mode", "ALL")
-    # Email to notify events from the scheduler
-    traj.f_add_parameter_to_group("JUBE_params", "mail_address",
-                                  "x@fz-juelich.de")
-    # Error file for the job
-    traj.f_add_parameter_to_group("JUBE_params", "err_file", "stderr")
-    # Output file for the job
-    traj.f_add_parameter_to_group("JUBE_params", "out_file", "stdout")
-    # JUBE parameters for multiprocessing. _to_evolveRelevant even without scheduler.
-    # MPI Processes per job
-    traj.f_add_parameter_to_group("JUBE_params", "tasks_per_job", "1")
-    # The execution command
-    traj.f_add_parameter_to_group("JUBE_params", "exec",
-                                  "srun -n 1 -c 8 --exclusive python " +
-                                  os.path.join(paths.root_dir_path,
-                                               "run-num-test",
-                                               "simulation",
-                                               "run_files",
-                                               "run_optimizee.py"))
-    # Ready file for a generation
-    traj.f_add_parameter_to_group("JUBE_params", "ready_file",
-                                  os.path.join(paths.root_dir_path,
-                                               "ready_files/ready_w_"))
-    # Path where the job will be executed
-    traj.f_add_parameter_to_group("JUBE_params", "work_path",
-                                  paths.root_dir_path)
-    traj.f_add_parameter_to_group("JUBE_params", "paths_obj", paths)
+    jube_params = {"exec": "srun -N 1 -n 1 -c 8 python"}
+    traj, params = experiment.prepare_experiment(trajectory_name=trajectory_name,
+                                                 jube_parameter=jube_params, name=name)
 
     # Resolve model and noise sources from input arguments
     if args.model == 'brunel':
@@ -121,14 +30,14 @@ def run_experiment(args):
             from comet.models.brunel.brunel_model import brunel_model as sim_model
         elif args.noise_type == 'pink':
             raise NotImplementedError('No pink noise for brunel model')
-    elif args.model == 'microcircuit':
-        if args.noise_type == 'poisson':
-            from comet_microcircuit_hyperparams import optimizee_params
-            from comet_microcircuit_hyperparams import optimizer_params
-            from comet.models.microcircuit.model_params import net_dict, bounds_dict
-            from comet.models.microcircuit.microcircuit_model import microcircuit_model as sim_model
-        elif args.noise_type == 'pink':
-            raise NotImplementedError('To be implemented')
+        elif args.model == 'microcircuit':
+            if args.noise_type == 'poisson':
+                from comet_microcircuit_hyperparams import optimizee_params
+                from comet_microcircuit_hyperparams import optimizer_params
+                from comet.models.microcircuit.model_params import net_dict, bounds_dict
+                from comet.models.microcircuit.microcircuit_model import microcircuit_model as sim_model
+            elif args.noise_type == 'pink':
+                raise NotImplementedError('To be implemented')
 
     # Resolve mode arg:
     if args.mode == 'syn':
@@ -147,24 +56,18 @@ def run_experiment(args):
         test_class=joint_test)
     # Inner-loop simulator
     optimizee = CometOptimizee(traj, optimizee_parameters)
-    jube.prepare_optimizee(optimizee, paths.root_dir_path)
-
-    logger.info("Optimizee parameters: %s", optimizee_parameters)
 
     # Outer-loop optimizer initialization
-    optimizer_seed = 123
-    pop_size = 24
     optimizer_parameters = GeneticAlgorithmParameters(
-                popsize=optimizer_params['popsize'],
-                seed=optimizer_params['seed'],
-                CXPB=optimizer_params['CXPB'],
-                MUTPB=optimizer_params['MUTPB'],
-                NGEN=optimizer_params['NGEN'],
-                indpb=optimizer_params['indpb'],
-                tournsize=optimizer_params['tournsize'],
-                matepar=optimizer_params['matepar'],
-                mutpar=optimizer_params['mutpar'])
-    logger.info("Optimizer parameters: %s", optimizer_parameters)
+        popsize=optimizer_params['popsize'],
+        seed=optimizer_params['seed'],
+        CXPB=optimizer_params['CXPB'],
+        MUTPB=optimizer_params['MUTPB'],
+        NGEN=optimizer_params['NGEN'],
+        indpb=optimizer_params['indpb'],
+        tournsize=optimizer_params['tournsize'],
+        matepar=optimizer_params['matepar'],
+        mutpar=optimizer_params['mutpar'])
 
     optimizer = GeneticAlgorithmOptimizer(traj,
                                           optimizee_create_individual=optimizee.create_individual,
@@ -173,23 +76,16 @@ def run_experiment(args):
                                           optimizee_fitness_weights=(-1,))
 
     # Add post processing
-    env.add_postprocessing(optimizer.post_process)
-
-    # Run the simulation with all parameter combinations
-    env.run(optimizee.simulate)
-
-    # Outer-loop optimizer end
-    optimizer.end()
-
-    # Finally disable logging and close all log-files
-    env.disable_logging()
-
+    experiment.run_experiment(optimizee=optimizee,
+                              optimizee_parameters=optimizee_parameters,
+                              optimizer=optimizer,
+                              optimizer_parameters=optimizer_parameters)
+    traj, paths = experiment.end(optimizer)
     return traj.v_storage_service.filename, traj.v_name, paths
 
 
 def main(args):
     filename, trajname, paths = run_experiment(args)
-    logger.info("Plotting now")
 
 
 if __name__ == '__main__':
